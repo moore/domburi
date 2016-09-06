057ff00d6c06e817b16b82ea0afbf8c63a0c28bb
Jonathan Moore <moore@eds.org>
2016-09-05 22:37:10 -0700

    encode tag names in to buffers. attrs should work too

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,13 +1,50 @@
 "use strict";
 
-main();
 
+//// String <-> ArrayBuffer convertion ////
+//Bug: This code only suports code points in the frist two pages
+//     so not big 5
+function ab2str (segment, offset, length) {
+    // BUG: I don't understand why Uint16Array.BYTES_PER_ELEMENT works?
+    // I thought this was suposed to be in bytes not elements!
+    return String.fromCharCode.apply(null, new Uint16Array(segment.buffer, offset, length/Uint16Array.BYTES_PER_ELEMENT));
+}
+
+function str2ab(str, segment, offset) {
+    var length = str2abLength(str);
+    var bufView = new Uint16Array(segment.buffer, offset, length);
+
+    for (var i=0, strLen=str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+
+    return offset + length
+}
+
+function str2abLength(str) {
+    return str.length * Uint16Array.BYTES_PER_ELEMENT;
+}
+
+
+//// Test code ////
 function make10( vdom, mkChildrenw ) {
+    var names = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+    };
+
     var ret = [];
     for ( var i = 0; i < 10; i++ ) {
         var children =  mkChildrenw? mkChildrenw(vdom) : [];
-        var ref = makeVnode( vdom, {name:i} );
-        ret.push(makeVtree(vdom, ref, children)); 
+        ret.push(makeVtree(vdom, {name:names[i], facts:{}}, children)); 
     }
     
     return ret;
@@ -16,8 +53,7 @@ function make10( vdom, mkChildrenw ) {
 function main( ) {
     var vdom = init();
     var children = make10(vdom, make10);
-    var ref = makeVnode( vdom, {name:'a'} );
-    var vnode = makeVtree( vdom, ref, children );
+    var vnode = makeVtree( vdom, {name:'a', facts:{}}, children );
     setRoot( vdom, vnode )
 
     printVdom( vdom )
@@ -30,9 +66,10 @@ function printVdom( vdom, ref, padding ) {
     if ( padding === undefined )
         padding = "";
     
-    var node = getNodeFromTree( vdom, ref );
+    var nodeIndex = readIndex( vdom, ref );
+    var tagName   = readTagName( vdom, ref )
     
-    console.log("%s::%s", padding, node.name );
+    console.log("%s::%s '%s'", padding, nodeIndex, tagName );
 
     var children = getChildren( vdom, ref );
 
@@ -74,7 +111,7 @@ function init ( ) {
         tree     : tree,
         data     : [data1],
         root     : -1,
-        nodes    : [],
+        maxIndex : 0,
         freeList : [],
     };
 }
@@ -105,7 +142,7 @@ var TREE_REF_SIZE = 4;
 [ child count * TREE_REF_SIZE ]
 */
 
-function makeVtree ( vdom, ref, children ) {
+function makeVtree ( vdom, facts, children ) {
 
     var tree  = vdom.tree;
     var head  = tree.head;
@@ -122,6 +159,8 @@ function makeVtree ( vdom, ref, children ) {
     
     var reference = head;
     var view      = tree.view;
+
+    var ref = makeVnode( vdom, facts );
 
     view.setUint32( head, vdom.epoc       ); head += 4; //EPOC_SIZE;
     view.setUint32( head, ref             ); head += 4; //DATA_REF_SIZE;
@@ -140,7 +179,12 @@ function makeVtree ( vdom, ref, children ) {
 [ epoc EPOC_SIZE ]
 [ index INDEX_SIZE ]
 DOMnode index // many vnodes can have the same dome node but not in a single tree
-facts...
+facts... ( format repeated hear for conveaonce )
+[uint16 fact count]
+( sorted by names, all uint32 size )
+[tag name end][name1 end][value1 end]...[nameN end][valueN end]
+[ ... fact data ... ]
+
 */
 
 function makeVnode ( vdom, node ) {
@@ -149,7 +193,7 @@ function makeVnode ( vdom, node ) {
     var head    = segment.head;
     var space   = segment.buffer.length;
 
-    var need = EPOC_SIZE + INDEX_SIZE ; //BOOG: I don't think this works
+    var need = EPOC_SIZE + INDEX_SIZE + factsLength( node.name, node.facts );
 
     // BUG: This is waistfull if a really big node almost fits. 
     if ( ( space - head ) < need ) {
@@ -167,18 +211,85 @@ function makeVnode ( vdom, node ) {
     }
     
     var offset = head;
-    var index  = storeNode( vdom, node );
+    var index  = getNodeIndex( vdom );
     var view   = segment.view;
     
     view.setUint32( head, vdom.epoc ); head += 4; //INDEX_SIZE
     view.setUint32( head, index     ); head += 4; //INDEX_SIZE
 
+    head = writeFacts(node.name, node.facts, segment, head);
+    
     segment.head = head;
 
     var reference = makeRef( last, offset );
 
     return reference;
 }
+
+/* -- facts -- 
+Note: We could have a uint 8 in hear that would
+tell us if we needed to use non ascii chars and
+use different widths for the data.
+Note: We could figure out the total length and pick the end pointer
+size appropetly.
+[uint16 fact count]
+( sorted by names, all uint32 size )
+[tag name end][name1 end][value1 end]...[nameN end][valueN end]
+[ ... fact data ... ]
+*/
+var FACT_COUNT_SIZE   = 2;
+var FACT_POINTER_SIZE = 4;
+
+function factsLength( tag, facts ) {
+
+    var length = FACT_COUNT_SIZE
+        + FACT_POINTER_SIZE
+        + str2abLength(tag);
+
+    var keys = Object.keys(facts);
+
+    length += keys.length * FACT_POINTER_SIZE * 2;
+
+    for ( var i = 0 ; i < keys.length ; i++ ) {
+        var key = keys[i];
+        length += str2abLength(key);
+        length += str2abLength(facts[key]);
+    }
+
+    return length;
+}
+
+function writeFacts(tag, facts, segment, offset) {
+    var view = segment.view
+    var indexOffset = offset + FACT_COUNT_SIZE;
+    var keys = Object.keys(facts);
+
+    keys.sort();
+    
+    offset += FACT_COUNT_SIZE + FACT_POINTER_SIZE
+        + keys.length * FACT_POINTER_SIZE * 2;
+
+    offset = str2ab(tag, segment, offset);
+
+    view.setUint32(indexOffset, offset)
+    indexOffset += FACT_POINTER_SIZE;
+
+    for ( var i = 0 ; i < keys.length ; i++ ) {
+        var key = keys[i];
+        offset = str2ab(key, segment, offset);
+
+        view.setUint32(indexOffset, offset)
+        indexOffset += FACT_POINTER_SIZE;
+
+        offset = str2ab(facts[key], segment, offset);
+        buffer.setUint32(indexOffset, offset)
+        indexOffset += FACT_POINTER_SIZE;
+    }
+
+    return offset
+}
+
+
 
 function setRoot ( vdom, reference ) {
     vdom.root = reference;
@@ -188,44 +299,71 @@ function getRoot ( vdom ) {
     return vdom.root;
 }
 
-function storeNode( vdom, node ) {
+function getNodeIndex ( vdom ) {
     var index;
     
     if ( vdom.freeList.length == 0 ) {
-        index = vdom.nodes.length;
-        vdom.nodes.push( node );
+        vdom.maxIndex += 1;
+        index = vdom.maxIndex;
     }
 
     else {
         index = vdom.freeList.pop();
-        vdom.nodes[ index ] = node;
     }
 
     return index;
 }
 
 
-function getNodeFromTree( vdom, reference ) {
-    var vnodeRef = vdom.tree.view.getUint32( reference + 4 );
-    return getNode( vdom, vnodeRef );
+function readIndex ( vdom, reference ) {
+    var vref = getVnodeRef( vdom, reference);
+    return readUint32( vdom, vref, EPOC_SIZE );
+}
+ 
+function readTagName ( vdom, reference ) {
+    var vref = getVnodeRef( vdom, reference);
+    var tagEnd = readUint32( vdom, vref, EPOC_SIZE + INDEX_SIZE + FACT_COUNT_SIZE );
+    var factCount = getFactCount( vdom, vref );
+    var segmentOffset = refOffset( vref );
+    var tagStart = segmentOffset + EPOC_SIZE + INDEX_SIZE + FACT_COUNT_SIZE + FACT_POINTER_SIZE + factCount * FACT_POINTER_SIZE;
+    var segment = getSegment( vdom, vref );
+    var tag = ab2str(segment, tagStart, tagEnd - tagStart);
+
+    return tag;
 }
 
-function getNode( vdom, reference ) {
-    var segmentIndex  = refSegment( reference );
-    var segmentOffset = refOffset( reference );
+function readUint32( vdom, vref, offset ) {    
+    var segment = getSegment( vdom, vref );
+    var segmentOffset = refOffset( vref );
     
-    var index = vdom.data[segmentIndex].view.getUint32(segmentOffset + 4); // EPOC_SIZE
-
-    return vdom.nodes[ index ];
+    return segment.view.getUint32(segmentOffset + offset );
 }
+
+function getFactCount( vdom, vref ) {
+    var segment = getSegment( vdom, vref );
+    var segmentOffset = refOffset( vref );
+    
+    return segment.view.getUint16(segmentOffset + EPOC_SIZE + INDEX_SIZE );
+}
+
+function getVnodeRef( vdom, reference ) {
+   return vdom.tree.view.getUint32( reference + EPOC_SIZE );
+}
+
+function getSegment( vdom, vref ) {
+    var segmentIndex  = refSegment( vref );
+    
+    return vdom.data[segmentIndex];
+}
+
 
 function getChildren( vdom, reference ) {
     var ret = [];
-    var ptr = reference + 4 + 4; // EPOC_SIZE + DATA_REF_SIZE
+    var ptr = reference + EPOC_SIZE + DATA_REF_SIZE;
     var view = vdom.tree.view;
-    var count = view.getUint32(ptr); ptr += 4; // COUNT_SIZE
+    var count = view.getUint32(ptr); ptr += COUNT_SIZE;
     for ( var i = 0; i < count ; i++ ) {
-        var childRef = view.getUint32(ptr); ptr += 4; // TREE_REF_SIZE
+        var childRef = view.getUint32(ptr); ptr += TREE_REF_SIZE;
         ret.push( childRef )
     }
     
@@ -255,3 +393,6 @@ function memcpy ( from, fOffset, to, tOffset, length ) {
 
     return true;
 }
+
+
+main();
